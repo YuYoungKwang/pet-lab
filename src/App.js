3cd974b1409e04514013f01b85af87302bf96921
YuYoungKwang <db99817@gmail.com>
2025-12-15 12:22:06 +0900

    헤더메뉴 기능, CSS  및 경로설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import { Routes, Route} from 'react-router';
 import './App.css';
-
+import Header from './components/common/Header';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header/>
+      <Routes>
+        {/* <Route path="/" element={<MainPage/>}/>
+        <Route path="/funding" element={<MainPage/>}/>
+        <Route path="/mypage" element={<MainPage/>}/>
+        <Route path="/login" element={<MainPage/>}/>
+        <Route path="/register" element={<Register/>}/> */}
+      </Routes>
     </div>
   );
 }
