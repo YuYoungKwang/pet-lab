651d08132703b7fdca04ad44090a07a0f4a5e884
YuYoungKwang <db99817@gmail.com>
2025-12-16 16:16:09 +0900

    펀딩등록 머지오류

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import FundingRegister from './pages/funding/FundingRegister';
 function App() {
   return (
     <div>
       <Header/>
       <Routes>
         {/* <Route path="/" element={<MainPage/>}/>
-        <Route path="/funding" element={<MainPage/>}/>
+        <Route path="/fundingregister" element={<FundingRegister/>}/>
         <Route path="/mypage" element={<MainPage/>}/>
         <Route path="/login" element={<MainPage/>}/>
         <Route path="/register" element={<Register/>}/> */}
+        <Route path="/funding" element={<FundingRegister/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/login" element={<Login/>}/>
       </Routes>
